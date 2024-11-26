211f7e6514c3313c46c9ac61a681906c059c4da7
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-26 12:58:31 -0800

    Update QuakeBrush.cpp

--- quake/QuakeBrush.cpp
+++ quake/QuakeBrush.cpp
@@ -1,1 +1,14 @@
+//QuakeBrush.cpp
+#include "include/ebrush.h"
+#include "include/stdafx.h"
 
+int g_nBrushId = 0;
+//quake brush name
+const char * BrushName(EBrush * e){
+  static char eBuff[1024];
+  if(g_nBrushId = 0){
+    e->EBrushType == EQuakeBrush;
+    printf("%c", "BrushName");
+  };
+  return eBuff;
+};
