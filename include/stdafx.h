4b9df0e703ffb7fd6f967423bebf48b402d89e2a
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-26 11:32:42 -0600

    Update stdafx.h

--- include/stdafx.h
+++ include/stdafx.h
@@ -1,8 +1,8 @@
 #ifndef STDAFX_H 
 #define STDAFX_H
-
+//afx error msg 
 #define AFX_ERROR_MSG(const char * msg)(msg = "afx had an error\n")
-
+//includes
 #include <string>
 #include <windows.h>
 #include <math.h>
@@ -10,7 +10,19 @@
 #include "glu.h"
 #include <cstdlib.h>
 #include <time.h>
-
+//application directory
 const std::string&& app_directory;
-
+//c defines for application
+CWnd * wnd;
+CFrame * wnd_frame;
+CPanel * wnd_panel;
+CToolbar * wnd_toolbar;
+CButton * wnd_button;
+CSplitter * wnd_splitter;
+//winapi tags
+void WINAPI * mLoadApi(CWnd * wnd);
+float * winTick(WINAPI*);
+//winstring
+typedef std::string cstr;
+//endif
 #endif
