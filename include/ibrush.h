a7ca1131e536e38d4bba5bd4c77f3041017ae2d5
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-26 09:50:04 -0600

    Update ibrush.h

--- include/ibrush.h
+++ include/ibrush.h
@@ -1,1 +1,12 @@
+#ifndef IBRUSHTABLE_H
+#define IBRUSHTABLE_H
+class IBrushTable{
+public:
 
+ IBrushTable();
+  ~IBrushTable();
+  typedef float ifloat;
+  IBrushTable * table;
+  ifloat MAX_BRUSH_NUM[10000];
+};
+#endif
