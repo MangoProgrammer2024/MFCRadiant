ec1edb29e44de2426881e45cafc96eaf12854973
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-26 13:33:42 -0800

    Update QuakeBrush.cpp

--- quake/QuakeBrush.cpp
+++ quake/QuakeBrush.cpp
@@ -6,9 +6,9 @@ int g_nBrushId = 0;
 //quake brush name
 const char * BrushName(EBrush * e){
   static char eBuff[1024];
-  if(g_nBrushId = 0){
-    e->EBrushType == EQuakeBrush;
-    printf("%c", "BrushName");
-  };
+    if(g_nBrushId = 0){
+      e->EBrushType == EQuakeBrush;
+      printf("%c", "BrushName");
+    };
   return eBuff;
 };
