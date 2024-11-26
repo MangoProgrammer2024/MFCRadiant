67a8ece50e2043109106bab66ae9d7ea2c7ef5e6
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-11-26 11:24:43 -0600

    Update stdafx.h

--- include/stdafx.h
+++ include/stdafx.h
@@ -3,5 +3,14 @@
 
 #define AFX_ERROR_MSG(const char * msg)(msg = "afx had an error\n")
 
+#include <string>
+#include <windows.h>
+#include <math.h>
+#include "GL.h"
+#include "glu.h"
+#include <cstdlib.h>
+#include <time.h>
+
+const std::string&& app_directory;
 
 #endif
